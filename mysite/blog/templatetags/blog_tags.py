daf772ab34e0ebb48c8789510d77eb4e8d30f216
Shatilov789 <realsteal1223@gmail.com>
2021-11-11 17:27:07 +0300

    Post,email,tags

---
+++ mysite/blog/templatetags/blog_tags.py
@@ -0,0 +1,19 @@
+from django import template
+from django.db.models import Count
+
+register = template.Library()
+
+from ..models import Post
+
+@register.simple_tag
+def total_posts():
+    return Post.objects.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.objects.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
